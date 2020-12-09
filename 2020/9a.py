3000a03f3e2ed653b867dfc70d5261875554e81c
Christoph Berg <myon@debian.org>
2020-12-09 11:38:30 +0100

    2020 Day 9

---
+++ 2020/9a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+xmas = []
+
+with open('9.txt') as f:
+    for line in f:
+        xmas.append(int(line))
+
+for p in range(25, len(xmas)):
+    found = False
+    for p1 in range(p-25, p):
+        for p2 in range(p1+1, p):
+            #print(f"{p}: {xmas[p1]} + {xmas[p2]} == {xmas[p1] + xmas[p2]} {xmas[p]}")
+            if xmas[p1] + xmas[p2] == xmas[p]:
+                print(f"{p}: {xmas[p1]} + {xmas[p2]} == {xmas[p]}")
+                found = True
+                break
+    if not found:
+        print('baaaad', p, xmas[p])
---
+++ 2020/9b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+xmas = []
+
+with open('9.txt') as f:
+    for line in f:
+        xmas.append(int(line))
+
+for p1 in range(len(xmas)):
+    for p2 in range(p1+1, len(xmas)):
+        if sum(xmas[p1:p2]) == 21806024:
+            print(p1, p2, xmas[p1:p2], min(xmas[p1:p2]) + max(xmas[p1:p2]))
