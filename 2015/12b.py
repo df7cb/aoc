aeeee246e23aa5adc093a33481b50ad2d802c6f6
Christoph Berg <myon@debian.org>
2020-12-10 21:49:28 +0100

    2015 Day 12

---
+++ 2015/12b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+
+with open('12.txt') as f:
+    js = json.loads(f.read())
+
+def parse(j):
+    sum = 0
+    if isinstance(j, dict):
+        for key in j:
+            if j[key] == 'red':
+                return 0 # part two
+            sum += parse(j[key])
+    elif isinstance(j, list):
+        for element in j:
+            sum += parse(element)
+    elif isinstance(j, str):
+        pass
+    else:
+        sum += j
+    return sum
+
+print(parse(js))
