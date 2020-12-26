3dd73c85ecb171dcd7c8ae45b650961301397edf
Christoph Berg <myon@debian.org>
2020-12-26 23:48:11 +0100

    2016 Day 17

---
+++ 2016/17a.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from collections import deque
+import hashlib
+
+#passcode = b'hijkl'
+#passcode = b'ihgpwlah'
+#passcode = b'kglvqrro'
+#passcode = b'ulqzkmiv'
+passcode = b'vwbaicqe'
+
+x0, y0 = 0, 0
+
+queue = deque([(0, '', x0, y0)]) # round number, path, x, y
+
+def open_doors(path):
+    md5 = hashlib.new('md5')
+    md5.update(passcode)
+    md5.update(str(path).encode('utf-8'))
+    hexdgt = md5.hexdigest()
+
+    out = set()
+    if hexdgt[0] in 'bcdef':
+        out.add('U')
+    if hexdgt[1] in 'bcdef':
+        out.add('D')
+    if hexdgt[2] in 'bcdef':
+        out.add('L')
+    if hexdgt[3] in 'bcdef':
+        out.add('R')
+    return out
+
+while len(queue) > 0:
+    r, path, x, y = queue.popleft()
+    doors = open_doors(path)
+    print(r, path, x, y, doors)
+    if x == y == 3:
+        break
+    for d, dx, dy in (('L', -1, 0), ('R', 1, 0), ('U', 0, -1), ('D', 0, 1)):
+        if d not in doors:
+            continue
+        x2, y2 = x+dx, y+dy
+        if x2 < 0 or x2 > 3 or y2 < 0 or y2 > 3:
+            continue
+
+        queue.append((r+1, path+d, x2, y2))
