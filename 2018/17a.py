69df9cadcc65bb80e5756025cd3f1ecd1681b3c5
Christoph Berg <myon@debian.org>
2021-12-12 14:10:09 +0100

    2018 Day 17 part a
    I believe this is correct, though the answer is too high

--- 2018/17a.py
+++ 2018/17a.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 import re
-import sys
-sys.setrecursionlimit(10000)
 
 #x=560, y=1729..1742
 #y=1288, x=483..485
@@ -12,10 +10,10 @@ range_x, range_y = 0, 0
 with open('17.txt') as f:
     for line in f:
         if m := re.match('x=(\d+), y=(\d+)\.\.(\d+)', line):
-            range_x = max(range_x, int(m.group(1)) + 1)
+            range_x = max(range_x, int(m.group(1)) + 2)
             range_y = max(range_y, int(m.group(3)) + 1)
         elif m := re.match('y=(\d+), x=(\d+)\.\.(\d+)', line):
-            range_x = max(range_x, int(m.group(3)) + 1)
+            range_x = max(range_x, int(m.group(3)) + 2)
             range_y = max(range_y, int(m.group(1)) + 1)
 
 ground = [list('.' * range_x) for y in range(range_y+1)]
@@ -37,56 +35,75 @@ ground[0][500] = '+'
 #    print(''.join(line))
 
 def flow(ground, y, x):
-    area, blocked = 0, False
-    if y == range_y - 1:
-        return area, blocked
-    # we've reached something down us blocking the flow
-    if y < range_y-1 and ground[y+1][x] in ('#', '~', 'l', 'r', '|'):
-        blocked = True
-    # flow down if spot is free
-    if y < range_y-1 and ground[y+1][x] == '.':
-        ground[y+1][x] = '|'
-        a2, blocked = flow(ground, y+1, x)
-        area += 1 + a2
-    # flow sidewards if something is blocking us
-    if y < range_y-1 and blocked: # or ground[y+1][x] not in ('.', '~', '|')):
-        #ground[y][x] = '~'
-        #blocked = ground[y][x-1] not in ('.', '|', 'l', 'r') and ground[y][x+1] not in ('.', '|', 'l', 'r')
+    area = 0
 
-        bl = True
-        xl = x-1
-        while ground[y][xl] == '.':
-            ground[y][xl] = 'l'
+    # flow down until we reach ground
+    yy = y + 1
+    while ground[yy][x] == '.':
+        area += 1
+        ground[yy][x] = '|'
+        yy += 1
+        if yy == range_y:
+            return area
+
+    if ground[yy][x] == '|': # flowing water discovered, stop here
+        return area
+
+    while True:
+        yy -= 1 # up one level (originally on the ground)
+        #assert(ground[yy][x] in ('|', '~'))
+        if ground[yy][x] == '.':
             area += 1
-            if ground[y+1][xl] == '.':
-                ground[y+1][xl] = '|'
-                a2, bl = flow(ground, y+1, xl)
-                area += 1 + a2
-                if not bl:
-                    break
+            ground[yy][x] = '|'
+
+        # flow to the left
+        xl = x - 1
+        bl = False
+        while ground[yy][xl] in ('.', '|', '~') and ground[yy+1][xl] not in ('.', '|'):
+            if ground[yy][xl] == '.':
+                area += 1
+                ground[yy][xl] = '|'
             xl -= 1
+        if ground[yy+1][xl] not in ('.', '|'): # no exit at this end
+            bl = True
+        else: # exit found
+            if ground[yy][xl] == '.':
+                area += 1
+                ground[yy][xl] = '|'
 
-        br = True
-        xr = x+1
-        while ground[y][xr] == '.':
-            ground[y][xr] = 'r'
-            area += 1
-            if ground[y+1][xr] == '.':
-                ground[y+1][xr] = '|'
-                a2, br = flow(ground, y+1, xr)
-                area += 1 + a2
-                if not br:
-                    break
+        # flow to the right
+        xr = x + 1
+        br = False
+        while ground[yy][xr] in ('.', '|', '~') and ground[yy+1][xr] not in ('.', '|'):
+            if ground[yy][xr] == '.':
+                area += 1
+                ground[yy][xr] = '|'
             xr += 1
+            if xr >= range_x:
+                break
+        if ground[yy+1][xr] not in ('.', '|'): # no exit at this end
+            br = True
+        else: # exit found
+            if ground[yy][xr] == '.':
+                area += 1
+                ground[yy][xr] = '|'
 
-        blocked = bl and br
-    return area, blocked
+        if not bl:
+            area += flow(ground, yy, xl)
 
-try:
-    area, blocked = flow(ground, 0, 500)
-    print(area, blocked)
-except:
-    pass
+        if not br:
+            area += flow(ground, yy, xr)
+
+        if bl and br: # no exit found, convert to settled water:
+            for xx in range(xl+1, xr):
+                ground[yy][xx] = '~'
+        else:
+            break
+
+    return area
+
+area = flow(ground, 0, 500)
+print(area)
 
 for line in ground:
     print(''.join(line))
