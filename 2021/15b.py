430114163d5665aa999377655cd2826a27bee6e8
Christoph Berg <myon@debian.org>
2021-12-15 16:09:56 +0100

    2021 Day 15: More datatype tweaks, 1.0s

--- 2021/15b.py
+++ 2021/15b.py
@@ -21,24 +21,26 @@ for yy in range(5):
 
 def check(cavern, queue, dist, cost_here, x, y):
     cost_new = cost_here + cavern[y][x]
-    if dist[y][x] < 0 or cost_new < dist[y][x]:
+    d_yx = dist[y][x]
+    if d_yx < 0 or cost_new < d_yx:
         dist[y][x] = cost_new
-        heapq.heappush(queue, (cost_new, (x, y)))
+        heapq.heappush(queue, (cost_new, x, y))
 
 def dijkstra(cavern, dist):
-    queue = [(0, (0, 0))]
+    queue = [(0, 0, 0)] # cost, (x, y)
     heapq.heapify(queue)
 
     while len(queue) > 0:
-        cost, (x, y) = heapq.heappop(queue)
+        cost, x, y = heapq.heappop(queue)
+        cost_here = dist[y][x]
         if x > 0:
-            check(cavern, queue, dist, dist[y][x], x-1, y)
+            check(cavern, queue, dist, cost_here, x-1, y)
         if x < len(cavern[0]) - 1:
-            check(cavern, queue, dist, dist[y][x], x+1, y)
+            check(cavern, queue, dist, cost_here, x+1, y)
         if y > 0:
-            check(cavern, queue, dist, dist[y][x], x, y-1)
+            check(cavern, queue, dist, cost_here, x, y-1)
         if y < len(cavern) - 1:
-            check(cavern, queue, dist, dist[y][x], x, y+1)
+            check(cavern, queue, dist, cost_here, x, y+1)
 
     return dist
 
