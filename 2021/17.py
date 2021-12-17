604fed105898da9a5f0a6bf0e123eb14e08a8465
Christoph Berg <myon@debian.org>
2021-12-17 13:07:55 +0100

    2021 Day 17

---
+++ 2021/17.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import re
+
+inp = 'target area: x=240..292, y=-90..-57'
+#inp = 'target area: x=20..30, y=-10..-5'
+
+m = re.match('target area: x=([\d-]+)..([\d-]+), y=([\d-]+)..([\d-]+)', inp)
+x1, x2, y1, y2 = [int(x) for x in m.groups()]
+
+def run(dx, dy):
+    x, y = 0, 0
+    max_y = 0
+    while True:
+        x += dx
+        y += dy
+        if dx > 0:
+            dx -= 1
+        dy -= 1
+
+        max_y = max(max_y, y)
+
+        #print(x, y)
+        if y < y1 or x > x2:
+            return False, max_y
+
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            return True, max_y
+
+working = 0
+max_max_y = 0
+for dy in range(y1, abs(x2*y1)): # FIXME: reasonable upper bound
+    #print(dy)
+    for dx in range(x2+1): # can't shoot harder than that or we overshoot in the first step
+        hit, max_y = run(dx, dy)
+        if hit and max_y > max_max_y:
+            max_max_y = max_y
+        if hit:
+            working += 1
+            print(working, dx, dy, hit, max_y, max_max_y)
+    dy += 1
+
