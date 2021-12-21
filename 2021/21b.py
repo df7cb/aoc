a7ec3af93b046c2a90f858ddc63458b3583a3ec0
Christoph Berg <myon@debian.org>
2021-12-21 19:31:12 +0100

    2021 Day 21 b finished

--- 2021/21b.py
+++ 2021/21b.py
@@ -3,8 +3,8 @@
 #Player 1 starting position: 6
 #Player 2 starting position: 7
 
-#pos = [6, 7]
-pos = [4, 8]
+pos = [6, 7]
+#pos = [4, 8]
 
 configs = {} # turn, pos0, pos1, score0, score1
 win = 21
@@ -12,6 +12,13 @@ win = 21
 def mod10(x):
     x = x % 10
     return 10 if x == 0 else x
+
+dice = []
+for d1 in range(1, 4):
+    for d2 in range(1, 4):
+        for d3 in range(1, 4):
+            dice.append(d1+d2+d3)
+print("Dice are", dice)
 
 def universes(turn, pos0, pos1, score0, score1):
     #print("Looking at", turn, pos0, pos1, score0, score1)
@@ -23,28 +30,21 @@ def universes(turn, pos0, pos1, score0, score1):
     elif score1 >= win:
         return (0, 1)
 
-    elif turn == 2:
+    elif turn == 0:
         s = (0, 0)
-        for die in range(1, 4):
+        for die in dice:
             pos0_ = mod10(pos0+die)
-            res = universes((turn+1)%6, pos0_, pos1, score0+pos0_, score1)
+            res = universes(1, pos0_, pos1, score0+pos0_, score1)
             s = (s[0]+res[0], s[1]+res[1])
         configs[(turn, pos0, pos1, score0, score1)] = s
         return s
-    elif turn == 5:
+    elif turn == 1:
         s = (0, 0)
-        for die in range(1, 4):
+        for die in dice:
             pos1_ = mod10(pos1+die)
-            res = universes((turn+1)%6, pos0, pos1_, score0, score1+pos1_)
+            res = universes(0, pos0, pos1_, score0, score1+pos1_)
             s = (s[0]+res[0], s[1]+res[1])
         configs[(turn, pos0, pos1, score0, score1)] = s
-        return s
-    else:
-        s = (0, 0)
-        for die in range(1, 4):
-            res = universes((turn+1)%6, pos0, pos1, score0, score1)
-            s = (s[0]+res[0], s[1]+res[1])
-        #configs[(turn, pos0, pos1, score0, score1)] = s
         return s
 
 res = universes(0, pos[0], pos[1], 0, 0)
