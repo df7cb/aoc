c8dceba6e602624ecb494f0c629f86af4f686c4e
Christoph Berg <myon@debian.org>
2021-12-21 22:49:12 +0100

    2018 Day 20 a: Algorithm works, but isn't efficient enough yet to terminanate

--- 2018/20a.py
+++ 2018/20a.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+from collections import defaultdict
+import heapq
+
+# the regexp is long
 import sys
 sys.setrecursionlimit(10000)
 
@@ -9,7 +13,6 @@ with open('20.txt') as f:
 print(regexp)
 
 def make_graph(regexp):
-
     head = 0
     heads = [0]
     branches = [0]
@@ -60,27 +63,60 @@ def make_graph(regexp):
 
 graph = make_graph(regexp)
 
-def walk(graph, id, prefix):
-    path = prefix
+print("Graph nodes:", len(graph))
+
+doors = defaultdict(set)
+x, y = 0, 0
+visited = set()
+
+def walk(graph, id, x, y):
+    if (id, x, y) in visited:
+        return
+    visited.add((id, x, y))
+
     node = graph[id]
     if node[0] is not None:
-        path += node[0]
+        if node[0] == 'N':
+            doors[x,y].add((x,y-1))
+            doors[x,y-1].add((x,y))
+            y -= 1
+        elif node[0] == 'S':
+            doors[x,y].add((x,y+1))
+            doors[x,y+1].add((x,y))
+            y += 1
+        elif node[0] == 'W':
+            doors[x,y].add((x-1,y))
+            doors[x-1,y].add((x,y))
+            x -= 1
+        elif node[0] == 'E':
+            doors[x,y].add((x+1,y))
+            doors[x+1,y].add((x,y))
+            x += 1
 
-    if node[1] == []:
-        print("Ended", path)
-    else:
-        for n in node[1]:
-            walk(graph, n, path)
+    for n in node[1]:
+        walk(graph, n, x, y)
 
-walk(graph, 0, '')
+walk(graph, 0, 0, 0)
 
-#    for p in range(start, end):
-#        if regexp[p] == 'N':
-#            y -= 1
-#        elif regexp[p] == 'S':
-#            y += 1
-#        elif regexp[p] == 'W':
-#            x -= 1
-#        elif regexp[p] == 'E':
-#            x += 1
-#    print("Walked to", x, y)
+print("Doors:", doors)
+
+def dijkstra(doors):
+    queue = [(0, 0, 0)] # cost here, (x, y)
+    heapq.heapify(queue)
+    cost = {(0, 0): 0}
+
+    while len(queue) > 0:
+        c, x, y = heapq.heappop(queue)
+        cost_here = cost[x, y]
+        for node in doors[x, y]:
+            if node not in cost or cost[node] > cost_here + 1:
+                cost[node] = cost_here + 1
+                heapq.heappush(queue, (cost_here + 1, node[0], node[1]))
+
+    return cost
+
+cost = dijkstra(doors)
+
+print("Cost:", cost)
+
+print("Furthest node distance:", max(cost.values()))
