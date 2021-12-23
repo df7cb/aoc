55ef977b7d8bb6cb7b9f31bc08a8cc39cfa605fe
Christoph Berg <myon@debian.org>
2021-12-23 18:14:46 +0100

    WIP: 2021 Day 23 a

--- 2021/23a.py
+++ 2021/23a.py
@@ -1,55 +1,121 @@
 #!/usr/bin/python3
 
-#############
-#01.2.3.4.56#
+###########1#
+#01.3.5.7.90#
 ###D#C#A#B###
   #C#D#A#B#
   #########
 
-hallway = ['.' for x in range(7)]
 chambers = [['C', 'D'], ['D', 'C'], ['A', 'A'], ['B', 'B']]
 
+#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
+
+chambers = [['A', 'B'], ['D', 'C'], ['C', 'B'], ['A', 'D']]
+
 want_chambers = [['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'D']]
+hallway = ['.' for x in range(11)]
+hallway_places = [0, 1, 3, 5, 7, 9, 10]
 
 print(hallway)
 print(chambers)
 
-def reachable_hallway(hallway, ch_exit):
+def is_free(hallway, a, b):
+    l, r = min(a,b), max(a,b)
+    #print("is_free", hallway, a, b, l, r)
+    for x in [x for x in range(l+1, r)] + [b]:
+        if hallway[x] != '.':
+            return False
+    return True
+
+def chamber_exit(ch):
+    return 2 * (ch+1)
+
+def reachable_hallway(hallway, ch):
+    assert(0 <= ch <= 3)
+    ch_exit = chamber_exit(ch)
     ha = []
-    for h in range(ch_exit): # check way to the left
-        if hallway[0:ch_exit] == ['.'] * ch_exit:
-            ha.append(h)
-    for h in range(ch_exit, 7): # check way to the right
-        if hallway[ch_exit:7] == ['.'] * (7-ch_exit):
+    for h in hallway_places:
+        if is_free(hallway, ch_exit, h):
             ha.append(h)
     return ha
 
-def chamber_exit(ch):
-    return ch + 2
+def reachable_chamber(hallway, n, ch):
+    ch_exit = chamber_exit(ch)
+    return is_free(hallway, n, ch_exit)
 
-def amphipod(a):
+def hallway_cost(ha, ch):
+    ch_exit = chamber_exit(ch)
+    c = abs(ha - ch_exit)
+    return c + 1 # one extra for moving to chamber
+
+def amphipod_cost(a):
     if a == 'A':
         return 1
-    elif a = 'B':
+    elif a == 'B':
         return 10
-    elif a = 'C':
+    elif a == 'C':
         return 100
-    else:
+    elif a == 'D':
         return 1000
+    else:
+        assert(0)
 
-def walk(hallway, chambers, cost):
+best_cost = 100000
+def walk(hallway, chambers, cost, level):
     if chambers == want_chambers:
-        print("Found solution with cost", cost)
+        global best_cost
+        if cost < best_cost:
+            best_cost = cost
+            print("Found solution with cost", cost, "best so far:", best_cost)
         return
 
+    if level <= 3:
+        print("    "*level + "Looking at", hallway, chambers, cost, best_cost)
+    # move one amphipod from the chambers to the hallway
     for ch in range(len(chambers)):
         if len(chambers[ch]) > 0:
-            for ha in reachable_hallway(hallway, chamber_exit(ch)):
-                amphipod = chambers[ch][-1]
-                print("Moving", chambers[ch][-1], "to", ha)
-                walk(hallway[:ha]+[chambers[ch][-1]]+hallway[ha+1:],
-                        chambers[:ch] + [chambers[ch][:-1]] + chambers[ch+1:],
-                        cost + amphipod_cost(
+            #print("    "*level + "for chamber", ch, "reachable hallway is", reachable_hallway(hallway, ch))
+            for ha in reachable_hallway(hallway, ch):
+                new_hallway = [x for x in hallway]
+                new_chambers = [[x for x in ch] for ch in chambers]
 
-walk(hallway, chambers, 0)
+                amphipod = new_chambers[ch].pop()
+                amphipod_n = ord(amphipod) - 65
+                if amphipod_n == ch and not (len(new_chambers[ch]) == 1 and new_chambers[ch][0] != ch):
+                    break # don't move out of target chamber unless blocking something
+                new_hallway[ha] = amphipod
+                new_cost = hallway_cost(ha, ch)
+                if len(new_chambers[ch]) == 0:
+                    new_cost += 1
+                new_cost *= amphipod_cost(amphipod)
+                #print("    "*level + "Moving", amphipod, "to", ha, "with cost", new_cost)
+                walk(new_hallway, new_chambers, cost + new_cost, level+1)
 
+    # move one amphipod from the hallway to the chambers
+    for ha in range(len(hallway)):
+        if hallway[ha] == '.':
+            continue
+        amphipod = hallway[ha]
+        amphipod_n = ord(amphipod) - 65
+        #print("    "*level + "Considering amphipod", amphipod, "at position", ha)
+        if (len(chambers[amphipod_n]) == 0 or \
+           len(chambers[amphipod_n]) == 1 and chambers[amphipod_n][0] == amphipod) and \
+           reachable_chamber(hallway, ha, amphipod_n):
+               new_hallway = [x for x in hallway]
+               new_chambers = [[x for x in ch] for ch in chambers]
+               new_hallway[ha] = '.'
+               new_chambers[amphipod_n].append(amphipod)
+               new_cost = hallway_cost(ha, ch)
+               if len(new_chambers[ch]) == 1:
+                   new_cost += 1
+               new_cost *= amphipod_cost(amphipod)
+               #print("    "*level + "Amphipod", amphipod, "can move to chamber", amphipod_n, "at cost", new_cost)
+               walk(new_hallway, new_chambers, cost + new_cost, level+1)
+
+walk(hallway, chambers, 0, 0)
+
+print("Found best solution with cost", best_cost)
