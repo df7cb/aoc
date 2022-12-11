420b8090f78406eeadfc7df824f404d5f4f5effb
Christoph Berg <myon@debian.org>
2022-12-11 11:18:02 +0100

    2022 Day 11

---
+++ 2022/11b.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import re
+
+monkeys = []
+modulus = 1
+
+with open("11.txt") as f:
+    for line in f:
+        if re.match('^Monkey', line):
+            monkeys.append({'inspect': 0})
+        elif m := re.search('Starting items: (.*)', line):
+            monkeys[-1]['items'] = eval(f'[{m.group(1)}]')
+        elif m := re.search('Operation: (.*)', line):
+            monkeys[-1]['op'] = m.group(1)
+        elif m := re.search('Test: divisible by (\d+)', line):
+            monkeys[-1]['div'] = int(m.group(1))
+            modulus *= int(m.group(1))
+        elif m := re.search('(true|false): throw to monkey (\d+)', line):
+            monkeys[-1][m.group(1) == 'true'] = int(m.group(2))
+
+print(monkeys)
+
+for turn in range(10000):
+    for monkey in monkeys:
+        for old in monkey['items']:
+            eval(compile(monkey['op'], '11a.py', 'single'))
+            new = new % modulus
+            next_monkey = monkey[new % monkey['div'] == 0]
+            monkeys[next_monkey]['items'].append(new)
+            monkey['inspect'] += 1
+        monkey['items'] = []
+    if turn % 1000 == 0:
+        print(monkeys)
+
+print(monkeys)
+
+inspects = [monkey['inspect'] for monkey in monkeys]
+inspects.sort()
+print(inspects)
+print(inspects[-2] * inspects[-1])
