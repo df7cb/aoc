8951f38e696b8d48a9a0a815c71800abc5162ea2
Christoph Berg <myon@debian.org>
2023-12-20 14:03:20 +0100

    WIP: 2023 Day 20b

---
+++ 2023/20b.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from collections import deque
+
+import re
+
+module = {'output': {'type': '', 'out': [], 'in': {}}}
+
+with open("20.txt") as f:
+    for line in f:
+        m = re.match('([&%]?)(\w+) -> (.*)', line)
+        module[m.group(2)] = {
+                'type': m.group(1),
+                'out': m.group(3).split(', '),
+                'in': {},
+                }
+        if m.group(1) == '%':
+            module[m.group(2)]['state'] = 0
+
+for m in [x for x in module]:
+    for out in module[m]['out']:
+        if out not in module:
+            module[out] = {'type': '', 'out': [], 'in': {}}
+
+        module[out]['in'][m] = 0
+
+count = {0: 0, 1: 0}
+
+def push_button():
+    signals = deque([('button', 0, 'broadcaster')])
+
+    while signals:
+        source, signal, dest = signals.popleft()
+        if dest == 'rx':
+            #print(f"{source} -{'high' if signal else 'low'}-> {dest} {md}")
+            if signal == 0:
+                return False
+        count[signal] += 1
+        md = module[dest]
+        #print(f"{source} -{'high' if signal else 'low'}-> {dest} {md}")
+        if md['type'] == '':
+            for out in md['out']:
+                signals.append((dest, signal, out))
+        elif md['type'] == '%':
+            if signal == 0:
+                md['state'] = 1 - md['state']
+                for out in md['out']:
+                    signals.append((dest, md['state'], out))
+        elif md['type'] == '&':
+            md['in'][source] = signal
+            memory = min(md['in'].values())
+            for out in md['out']:
+                signals.append((dest, 1 - memory, out))
+
+    #print()
+    return True
+
+presses = 1
+while push_button():
+    if presses % 10000 == 0:
+        print(presses)
+    presses += 1
+
+print(presses)
