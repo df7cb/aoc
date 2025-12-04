60bc423d9677cd0bcf94433371a6b1dc6f9b0deb
Christoph Berg <myon@debian.org>
2025-12-04 16:43:33 +0100

    2025 day 04

---
+++ 2025/04b.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+with open("04.txt") as f:
+    grid = [list(line.strip()) for line in f]
+
+for line in grid:
+    print(line)
+
+print()
+
+lift_rolls = 0
+old_lift_rolls = -1
+
+while lift_rolls != old_lift_rolls:
+    old_lift_rolls = lift_rolls
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == '.': continue
+            rolls = 0
+            for dy in (-1, 0, 1):
+                if y + dy < 0 or y + dy >= len(grid): continue
+                for dx in (-1, 0, 1):
+                    if (dy, dx) == (0, 0): continue
+                    if x + dx < 0 or x + dx >= len(grid[0]): continue
+                    if grid[y+dy][x+dx] != '.':
+                        rolls += 1
+
+            if rolls < 4:
+                if grid[y][x] == '@':
+                    grid[y][x] = '.'
+                    lift_rolls += 1
+
+    for line in grid:
+        print(line)
+
+print(lift_rolls)
